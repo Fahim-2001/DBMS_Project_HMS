c5edb32370df652312cf14aa1f1fe28d3e6cf4e8
Fahim2001 <mdyasirofficial2001@gmail.com>
2023-10-25 01:05:40 +0600

    Lab Test Requests API integrated with database

--- src/app/api/labtestrequests/route.js
+++ src/app/api/labtestrequests/route.js
@@ -6,10 +6,10 @@ const connection = await pool.getConnection();
 // Doctor By ID api
 export async function GET(req) {
   try {
-    // const [data] = await connection.query("SELECT*FROM vaccineforms");
-    // connection.release();
+    const [data] = await connection.query("SELECT*FROM lab_test_requests");
+    connection.release();
     // console.log(data);
-    const data=[]
+    
     return NextResponse.json(data, { status: 200 });
   } catch (error) {
     return NextResponse.json(error.message, { status: 500 });
@@ -20,8 +20,10 @@ export async function POST(req) {
   try {
     const labtest = await req.json();
     console.log(labtest)
+    const tests = JSON.stringify(labtest?.tests);
     
-
+    await connection.query('INSERT INTO lab_test_requests(fullname,age,gender,contact,email,number_of_tests, tests,registered_by,registers_email) VALUES(?,?,?,?,?,?,?,?,?)',[labtest?.fullname,labtest?.age,labtest?.gender,labtest?.contact,labtest?.email,labtest?.number_of_tests,tests,labtest?.registered_by,labtest?.registers_email]);
+    connection.release()
     return NextResponse.json(
       { message: "Lab Test submission done!" },
       { status: 201 }
