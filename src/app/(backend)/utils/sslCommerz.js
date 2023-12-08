3a5583615a0fe3e41b24d8f971229c310558dcfd
Fahim2001 <mdyasirofficial2001@gmail.com>
2023-12-09 02:39:08 +0600

    Deployment issue: Removed Src folders except backend to deploy

--- src/app/(backend)/utils/sslCommerz.js
+++
@@ -1,4 +0,0 @@
-export const SSLCommerzPayment = require('sslcommerz-lts')
-export const store_id = process.env.SSLCOMMERZ_STORE_ID
-export const store_passwd = process.env.SSLCOMMERZ_STORE_PASS
-export const is_live = false //true for live, false for sandbox
