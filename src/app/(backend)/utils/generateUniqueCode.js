8ba095d45ba364c96821bf194c210d5a45438115
Fahim2001 <mdyasirofficial2001@gmail.com>
2023-12-09 02:44:04 +0600

    Fixed 1

--- src/app/(backend)/utils/generateUniqueCode.js
+++
@@ -1,9 +0,0 @@
-// Function To generate OTP
-export function generateUniqueCode(length = 6) {
-    const digits = "0123456789";
-    let OTP = "";
-    for (let i = 0; i < length; i++) {
-      OTP += digits[Math.floor(Math.random() * 10)];
-    }
-    return OTP;
-  }
--- src/app/(backend)/utils/nodemailer.js
+++
@@ -1,9 +0,0 @@
-const nodemailer = require('nodemailer');
-
-export const transporter = nodemailer.createTransport({
-    service:'gmail',
-    auth:{
-        user: process.env.EMAIL,
-        pass: process.env.PASS
-    }
-})
